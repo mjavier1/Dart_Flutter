8dce50e3d464409e521408715541aa1f1ea3d40b
mjavier1 <mjaguirre5@gmail.com>
2021-06-13 21:17:40 -0400

    bool
    true and false

---
+++ Bool/bool.dart
@@ -0,0 +1,17 @@
+void main() {
+
+  //bool
+  //
+   // print("hello");
+  //change variable true and false
+  
+bool ison=true;
+print('ison=${ison}');
+ison=false;
+print('ison=${ison}');
+print('is on is a ${ison.runtimeType}');   
+//  
+
+
+
+}
