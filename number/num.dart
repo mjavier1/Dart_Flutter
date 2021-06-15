680a1a393da03719d41c774d6d228bd5a045424e
mjavier1 <mjaguirre5@gmail.com>
2021-06-15 15:10:12 -0400

    variable is number basic
    math

---
+++ number/num.dart
@@ -0,0 +1,24 @@
+void main(){
+ //variable is basic 
+  
+  double people =0;
+ int  age=34;
+// int people=0;
+ double tem=32.06;
+  
+  //int num people="hello";
+
+  print("n");
+  print(people);
+ int test=int.parse('12');
+ int err = int.parse('12.09',onError:(source)=>0);
+ 
+ print(test);
+ print('err = ${err}');
+ int dog=7;
+int od = age*dog;
+print("your age in dog years is : ${od}");
+  
+  
+  
+}
