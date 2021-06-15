fe39df014d93715c700ded7bab20038760d4c3ef
mjavier1 <mjaguirre5@gmail.com>
2021-06-15 16:39:51 -0400

    Create input.dart

---
+++ intput/input.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+
+void main()
+{
+	// Printing in first way
+//input stdout
+
+	// Printing in second way
+	stdout.write("Welcome to GeeksforGeeks! // printing from stdout.write()");
+    String name=stdin.readLineSync();
+    print(name);
+    
+}
