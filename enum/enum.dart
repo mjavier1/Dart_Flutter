6670dfff44224f64113b37cb6d35949305e5a687
mjavier1 <mjaguirre5@gmail.com>
2021-06-17 11:12:28 -0400

    add print colors

--- enum/enum.dart
+++ enum/enum.dart
@@ -4,6 +4,7 @@ void main()
 //array with variable different
   //https://www.tutorialspoint.com/dart_programming/dart_programming_enumeration.htm
   print(colors.values);
+	print(colors.red);
 	// Printing in first way
 
 
