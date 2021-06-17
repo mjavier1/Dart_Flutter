9ea0ca5d8d6e620176f3c699b889faa36c1d2994
mjavier1 <mjaguirre5@gmail.com>
2021-06-17 11:00:48 -0400

    Create inputtwo.dart

---
+++ inputtwo.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+import 'dart:async';
+
+void main() {
+
+  print('What is your first name?');
+  String name = stdin.readLineSync();
+
+  print('What is your last name?');
+  String last = stdin.readLineSync();
+
+  //last and name
+  print('Hello ${firstname} ${lastname}');
+
+
+}
