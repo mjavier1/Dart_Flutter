9b93afdebbf669a72125b069dcfe50a1f7403083
mjavier1 <mjaguirre5@gmail.com>
2021-06-17 12:10:01 -0400

    set number list

---
+++ List_array/set.dart
@@ -0,0 +1,10 @@
+void main(){
+  Set <int> numbers = new Set<int>();
+  numbers.add(1);
+    numbers.add(2);
+    numbers.add(3);
+    numbers.add(2);
+  //no always number in exit is numbers
+  print(numbers);
+  
+}
