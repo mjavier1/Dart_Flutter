5a1190760748f3b2c626c1e90b39224c74515a77
mjavier1 <mjaguirre5@gmail.com>
2021-06-17 11:10:43 -0400

    enum

---
+++ enum/enum.dart
@@ -0,0 +1,12 @@
+enum colors{red,green,blue}
+void main()
+{
+//array with variable different
+  //https://www.tutorialspoint.com/dart_programming/dart_programming_enumeration.htm
+  print(colors.values);
+	// Printing in first way
+
+
+
+
+}
