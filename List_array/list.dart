a0c1a4b6e0946504fd49267f9aaf48f810f5dcde
mjavier1 <mjaguirre5@gmail.com>
2021-06-17 11:44:04 -0400

    list

---
+++ List_array/list.dart
@@ -0,0 +1,21 @@
+void main(){
+  List test =[0,1,2,3,4];
+  print('length = ${test.length}');
+  print('frist item is ${test[0]}');
+  //print(test.elementAt(1321));
+  List things = new List();
+  things.add(1);
+  things.add('cats');
+  things.add(true);
+  print(things);
+ List<int> numbers = new List<int>();
+    
+  numbers.add(1);
+  numbers.add(2);
+  numbers.add(3);
+  //numbers.add('cats'); - ERROR !
+  
+  //print(test.length);
+ // print("First item");
+  
+}
