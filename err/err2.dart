7aa77b892beeed40ee7e43d94154a1e71c2edc9f
mjavier1 <mjaguirre5@gmail.com>
2021-06-20 14:22:10 -0400

    Create err2.dart

---
+++ err/err2.dart
@@ -0,0 +1,14 @@
+//try catch
+void main(){
+  try{
+    int age;
+    int dog=7;
+    
+  }catch(e){
+   print('there was an error: ${e.toString()}'); 
+  }
+    
+    finally{
+      print('complete');
+    }
+}
