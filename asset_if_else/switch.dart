7106a3eb013a6a1de38d9d0dd1010e6dfef82a4c
mjavier1 <mjaguirre5@gmail.com>
2021-06-20 11:26:06 -0400

    case

---
+++ asset_if_else/switch.dart
@@ -0,0 +1,17 @@
+void main() {
+ int age=65;
+  switch(age){
+    case 18:
+      print('you are 18, you can vote');
+      break;
+    case 21:
+      print('you are 21, you an adult');
+      break;
+    case 65:
+      print('you are 65, you can vote');
+      break;
+    default:
+      break;
+  }
+  print('finished');
+}
