b062f41a3d61c06418ad69991a092d4773f24848
mjavier1 <mjaguirre5@gmail.com>
2021-06-20 14:26:42 -0400

    error

--- err/err2.dart
+++ err/err2.dart
@@ -12,3 +12,29 @@ void main(){
       print('complete');
     }
 }
+/*
+main(List<String> arguments) {
+
+  //Error is a program failure
+  //Exemption - can be handled
+
+
+  try {
+    int age;
+    int dogyears = 7;
+
+    print(age * dogyears);
+  }
+  on NoSuchMethodError {
+    print('Sorry thats not going to happen');
+  }
+  catch (e) {
+    print('There was an error: ${e.toString()}');
+  }
+  finally {
+    print('complete');
+  }
+
+}
+
+*/
